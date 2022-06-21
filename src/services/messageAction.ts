0f5533d75fbc447d917e3396dc18c94caf4fd4ad
Manish Mandal <manish.mandal@layrins.com>
2022-06-21 17:13:22 +0530

    degugging the response msg

--- src/services/messageAction.ts
+++ src/services/messageAction.ts
@@ -4,13 +4,17 @@ import { messageView } from '../view/message';
 // Post a message to a channel your app is in using ID and message text
 const messageAction = async (app: App) => {
   try {
-    app.action('join_btn', async ({ body, ack, say }) => {
+    app.action('join_btn', async ({ body, ack, say, respond }) => {
       // Acknowledge the action
+      console.log(body);
       //   @ts-ignore
       console.log(body.message.blocks[3].elements[0].text);
       console.log('msg received');
       await ack();
-      await say(`<@${body.user.id}> clicked the button`);
+      //   await say(`<@${body.user.id}> clicked the button`);
+      await respond({
+        blocks: messageView(),
+      });
     });
 
     // Print result, which includes information about the message (like TS)
